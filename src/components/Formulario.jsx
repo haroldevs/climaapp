b2d409ceb34c58c7684678d596bc40695734bc52
haroldevs <haroldthomasg18@gmail.com>
2024-02-17 00:30:25 -0500

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import useClima from "../hooks/useClima";
+
+const Formulario = () => {
+  const [alerta, setAlerta] = useState("");
+  const { busqueda, datosBusqueda } = useClima();
+
+  const { ciudad, pais } = busqueda;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (Object.values(busqueda).includes("")) {
+      setAlerta("Todos los campos son obligatorios");
+      return;
+    }
+    consultarClima(busqueda);
+  };
+
+  return (
+    <div className="contenedor">
+      {alerta && <p>{alerta}</p>}
+      <form onSubmit={handleSubmit}>
+        <div className="campo">
+          <label htmlFor="ciudad">Ciudad</label>
+          <input
+            type="text"
+            id="ciudad"
+            name="ciudad"
+            value={ciudad}
+            onChange={datosBusqueda}
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="pais">País</label>
+          <select id="pais" name="pais" value={pais} onChange={datosBusqueda}>
+            <option value="">-- Seleccione un país</option>
+            <option value="US">Estados Unidos</option>
+            <option value="MX">México</option>
+            <option value="AR">Argentina</option>
+            <option value="CO">Colombia</option>
+            <option value="CR">Costa Rica</option>
+            <option value="ES">España</option>
+            <option value="PE">Perú</option>
+          </select>
+        </div>
+        <input type="submit" value={"Consulta Clima"} />
+      </form>
+    </div>
+  );
+};
+
+export default Formulario;
---
+++ src/context/ClimaProvider.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+
+const ClimaContext = createContext();
+
+const ClimaProvider = ({ children }) => {
+  const [busqueda, setBusqueda] = useState({ ciudad: "", pais: "" });
+
+  const datosBusqueda = (e) => {
+    setBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const consultarClima = (datos) => {
+    console.log(datos);
+  };
+
+  return (
+    <ClimaContext.Provider value={{ busqueda, datosBusqueda, consultarClima }}>
+      {children}
+    </ClimaContext.Provider>
+  );
+};
+
+export { ClimaProvider };
+
+export default ClimaContext;
